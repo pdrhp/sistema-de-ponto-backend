bc753c7c7874aa9006df7466fc7d9bfbc6a6fa58
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-24 20:12:50 -0300

    Creation of HttpPost in EmployeeController

--- sistema-de-ponto/Controllers/EmployeeController.cs
+++ sistema-de-ponto/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using sistema_de_ponto.Data;
 using sistema_de_ponto.Data.DTOs;
+using sistema_de_ponto.Models;
 
 namespace sistema_de_ponto.Controllers;
 
@@ -9,15 +11,22 @@ namespace sistema_de_ponto.Controllers;
 public class EmployeeController : ControllerBase
 {
     private SistemaDePontoContext _context;
+    private IMapper _mapper;
     
-    public EmployeeController(SistemaDePontoContext context)
+    public EmployeeController(SistemaDePontoContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
-    public IActionResult AddEmployee(CreateEmployeeDTO employeeDto)
+    [HttpPost]
+    public IActionResult AddEmployee([FromBody]CreateEmployeeDTO employeeDto)
     {
-        
+        Employee employee = _mapper.Map<Employee>(employeeDto);
+
+        _context.Employee.Add(employee);
+        _context.SaveChanges();
+        return Ok(employee);
     }
     
     
