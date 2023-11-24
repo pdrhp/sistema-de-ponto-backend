23008928f11c9f8e7b496c300a617b65ac7fdabf
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-24 19:56:54 -0300

    Creation Mapper profile to class Employee

--- sistema-de-ponto/Data/DTOs/ReadEmployeeDTO.cs
+++ sistema-de-ponto/Data/DTOs/ReadEmployeeDTO.cs
@@ -2,12 +2,16 @@
 
 namespace sistema_de_ponto.Data.DTOs;
 
-public class CreateEmployeeDTO
+public class ReadEmployeeDTO
 {
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    
     [Required(ErrorMessage = "O campo nome é obrigatorio")]
     [StringLength(200, MinimumLength = 5, ErrorMessage = "O campo nome deve ter entre 5 e 200 caracteres")]
     public string Name { get; set; }
-    
+
     [Required(ErrorMessage = "O campo email é obrigatorio")]
     [EmailAddress]
     public string Email { get; set; }
---
+++ sistema-de-ponto/Profiles/EmployeeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using sistema_de_ponto.Data.DTOs;
+using sistema_de_ponto.Models;
+
+namespace sistema_de_ponto.Profiles;
+
+
+public class EmployeeProfile : Profile
+{
+    public EmployeeProfile()
+    {
+        CreateMap<CreateEmployeeDTO, Employee>();
+        CreateMap<Employee, ReadEmployeeDTO>();
+        CreateMap<UpdateEmployeeDTO, Employee>();
+    }
+}
