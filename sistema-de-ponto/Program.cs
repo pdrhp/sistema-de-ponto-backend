ad4ac6094beab99f83eed06e9495a02285ab22ef
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-27 19:01:14 -0300

    Add AutoMapper Service to startup of API

--- sistema-de-ponto/Program.cs
+++ sistema-de-ponto/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<SistemaDePontoContext>(opts =>
     opts.UseMySql(connectionStringMySql,
         ServerVersion.Parse("8.0.21-mysql")));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
