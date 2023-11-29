8bcac260f5f937af31aa2f973a3b2d7ec33cf5a8
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-29 19:42:18 -0300

    Employee Service creation

---
+++ sistema-de-ponto/Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using sistema_de_ponto.Data;
+using sistema_de_ponto.Data.DTOs;
+using sistema_de_ponto.Models;
+
+namespace sistema_de_ponto.Services;
+
+public class EmployeeService : IEmployeeService
+{
+
+    private SistemaDePontoContext _context;
+    private IMapper _mapper;
+    
+    public EmployeeService(SistemaDePontoContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public Employee AddEmployee(Employee employee)
+    {
+        _context.Employee.Add(employee);
+        _context.SaveChanges();
+        return employee;
+    }
+
+    public Employee? GetEmployeeWithID(int id)
+    {
+        Employee employee = _context.Employee.FirstOrDefault(employee => employee.Id == id);
+        if (employee != null)
+        {
+            return employee;
+        }
+
+        return null;
+    }
+
+    public IEnumerable<Employee> GetAllEmployees()
+    {
+        return _context.Employee;
+    }
+
+    public void UpdateEmployee(UpdateEmployeeDTO employeeDto, Employee employee)
+    {
+        _mapper.Map(employeeDto, employee);
+        _context.SaveChanges();
+    }
+
+    public void DeleteEmployee(Employee employee)
+    {
+        _context.Employee.Remove(employee);
+        _context.SaveChanges();
+    }
+}
