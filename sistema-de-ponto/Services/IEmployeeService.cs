9af3f910889dd90b2f5fe974e09112874616d078
Pedro Silva <pedrohsp2004@gmail.com>
2023-11-29 19:42:09 -0300

    Creation of IEmployeeService Interface

---
+++ sistema-de-ponto/Services/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using sistema_de_ponto.Data.DTOs;
+using sistema_de_ponto.Models;
+
+namespace sistema_de_ponto.Services;
+
+public interface IEmployeeService
+{
+    Employee AddEmployee(Employee employee);
+    
+    Employee GetEmployeeWithID(int id);
+    
+    IEnumerable<Employee> GetAllEmployees();
+    
+    void UpdateEmployee(UpdateEmployeeDTO employeeDto,Employee employee);
+    
+    void DeleteEmployee(Employee employee);
+}
