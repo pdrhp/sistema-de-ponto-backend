91efbe7dd012166df72abfc83cc0072451dc33d6
Pedro Silva <pedrohsp2004@gmail.com>
2023-12-06 19:48:46 -0300

    Creation of TimeRecordService

---
+++ sistema-de-ponto/Services/ITimeRecordService.cs
@@ -0,0 +1,11 @@
+﻿using sistema_de_ponto.Models;
+
+namespace sistema_de_ponto.Services;
+
+public interface ITimeRecordService
+{
+    void AddTimeRecord(TimeRecord timeRecord);
+    TimeRecord GetLatestTimeRecordForEmployee(int employeeId);
+    void UpdateTimeRecord(TimeRecord timeRecord);
+    IEnumerable<TimeRecord> GetAllTimeRecords();
+}
---
+++ sistema-de-ponto/Services/TimeRecordService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using sistema_de_ponto.Data;
+using sistema_de_ponto.Models;
+
+namespace sistema_de_ponto.Services;
+
+public class TimeRecordService : ITimeRecordService
+{
+    private SistemaDePontoContext _context;
+    private IMapper _mapper;
+    private IEmployeeService _employeeService;
+
+    public TimeRecordService(SistemaDePontoContext context, IMapper mapper, IEmployeeService employeeService)
+    {
+        _context = context;
+        _mapper = mapper;
+        _employeeService = employeeService;
+    }
+    
+    
+    public void AddTimeRecord(TimeRecord timeRecord)
+    {
+        _context.TimeRecord.Add(timeRecord);
+        _context.SaveChanges();
+    }
+
+    public TimeRecord GetLatestTimeRecordForEmployee(int employeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTimeRecord(TimeRecord timeRecord)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TimeRecord> GetAllTimeRecords()
+    {
+        throw new NotImplementedException();
+    }
+}
